bef2ba7085a30eef51117500d38a688cdde3daa5
Edwin Cheng <edwincheng4840@gmail.com>
2024-01-09 13:53:38 -0500

    Add validations for account creation

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AbstractControl, FormControl, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms'
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  form: FormGroup
 
+  constructor(private router: Router) {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [Validators.required])
+    },
+    {
+      validators: this.passwordMatch
+    });
+  }
+
+  submitForm() {
+    console.log(this.form);
+    this.router.navigate(["home"]);
+  }
+
+  passwordMatch(control: AbstractControl) {
+    return control.get('password')?.value === control.get('confirmPassword')?.value ? null : {mismatch:true};
+  }
 }
