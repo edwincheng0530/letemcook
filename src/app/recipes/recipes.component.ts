a187dab60fa9e3412578794057a0402030737395
Edwin Cheng <edwin.cheng@stonybrook.edu>
2024-10-09 11:49:39 -0400

    Add popup component to recipes component

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 
 import { HeaderComponent } from '../header/header.component';
+import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 import { Recipe } from '../models/Recipe';
 import { RecipeService } from '../services/recipe.service';
 import { UsersService } from '../services/users.service';
@@ -15,12 +16,14 @@ import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [RouterModule, CommonModule, HeaderComponent],
+  imports: [RouterModule, CommonModule, HeaderComponent, ConfirmDialogComponent],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css'
 })
 export class RecipesComponent {
   recipes$!: Observable<Recipe[]>;
+  showModal = false;
+  recipeToDelete: number | null = null;
 
   constructor(private router: Router, private recipeService: RecipeService, private userService: UsersService) { }
 
@@ -34,20 +37,54 @@ export class RecipesComponent {
   }
 
   deleteRecipe(idrecipe: number) {
-    this.recipeService.deleteRecipe(idrecipe).pipe(
-      switchMap(response => {
-        console.log('Recipe successfully deleted:', response);
-        return this.recipeService.fetchAll(this.userService.getEmail()); // Assuming you want to fetch users after creating one
-      }),
-      catchError(error => {
-        console.error('Error deleting recipe:', error);
-        throw error; // Rethrow the error to propagate it to the next error handler
-      })
-    )
-    .subscribe(recipe => {
-      console.log('Deleted recipe:', recipe);
-      this.recipes$ = this.recipeService.fetchAll(this.userService.getEmail());
-    });
+    this.recipeToDelete = idrecipe; // Set the recipe ID for deletion
+    this.showModal = true; // Show the confirmation modal
   }
+  
+  onConfirmDelete() {
+    if (this.recipeToDelete !== null) {
+      this.recipeService.deleteRecipe(this.recipeToDelete).pipe(
+        switchMap(response => {
+          console.log('Recipe successfully deleted:', response);
+          return this.recipeService.fetchAll(this.userService.getEmail()); // Fetch updated recipes
+        }),
+        catchError(error => {
+          console.error('Error deleting recipe:', error);
+          throw error; // Rethrow the error to propagate it to the next error handler
+        })
+      )
+      .subscribe(recipes => {
+        console.log('Updated recipes list:', recipes);
+        this.recipes$ = this.recipeService.fetchAll(this.userService.getEmail()); // Update the recipes observable with the new list
+        this.showModal = false; // Close the modal
+        this.recipeToDelete = null; // Clear the recipe ID
+      });
+    }
+  }
+  onCancelDelete() {
+    this.showModal = false; // Close the modal without deleting
+    this.recipeToDelete = null; // Clear the recipe ID
+  }
+
+  // deleteRecipe(idrecipe: number) {
+  //   const confirmation = confirm('Are you sure you want to delete this recipe?');
+
+  //   if(confirmation) {
+  //     this.recipeService.deleteRecipe(idrecipe).pipe(
+  //       switchMap(response => {
+  //         console.log('Recipe successfully deleted:', response);
+  //         return this.recipeService.fetchAll(this.userService.getEmail()); // Assuming you want to fetch users after creating one
+  //       }),
+  //       catchError(error => {
+  //         console.error('Error deleting recipe:', error);
+  //         throw error; // Rethrow the error to propagate it to the next error handler
+  //       })
+  //     )
+  //     .subscribe(recipe => {
+  //       console.log('Deleted recipe:', recipe);
+  //       this.recipes$ = this.recipeService.fetchAll(this.userService.getEmail());
+  //     });
+  //   }
+  // }
 
 }
